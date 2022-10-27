dffcf1773d0f5369bec708e2c1d8a50c46312e2c
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-10-27 14:07:14 -0300

    Locale Español Chile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule }  from '@angular/common/http';
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PersonajesModule } from './personajes/personajes.module';
 import { EpisodiosModule } from './episodios/episodios.module';
+
+import localeChile from '@angular/common/locales/es-CL';
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeChile);
 
 @NgModule({
   declarations: [
@@ -20,7 +24,9 @@ import { EpisodiosModule } from './episodios/episodios.module';
     PersonajesModule,
     EpisodiosModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-CL' 
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
